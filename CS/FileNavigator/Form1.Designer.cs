d48979cc9ab2eade3efbd10035a065b6b5b4d713
Bogdan Kharchenko <bogdan.kharchenko@devexpress.com>
2025-09-21 15:13:14 +0400

    Source code updated

--- CS/FileNavigator/Form1.Designer.cs
+++ CS/FileNavigator/Form1.Designer.cs
@@ -26,27 +26,26 @@
             this.components = new System.ComponentModel.Container();
             DevExpress.XtraEditors.BreadCrumbNode breadCrumbNode1 = new DevExpress.XtraEditors.BreadCrumbNode();
             DevExpress.XtraEditors.BreadCrumbNode breadCrumbNode2 = new DevExpress.XtraEditors.BreadCrumbNode();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.breadCrumbEdit1 = new DevExpress.XtraEditors.BreadCrumbEdit();
             this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.sharedImageCollection1 = new DevExpress.Utils.SharedImageCollection(this.components);
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.breadCrumbEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sharedImageCollection1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sharedImageCollection1.ImageSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
             this.SuspendLayout();
             // 
             // breadCrumbEdit1
             // 
             this.breadCrumbEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.breadCrumbEdit1.Location = new System.Drawing.Point(12, 11);
+            this.breadCrumbEdit1.Location = new System.Drawing.Point(24, 21);
+            this.breadCrumbEdit1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.breadCrumbEdit1.Name = "breadCrumbEdit1";
             this.breadCrumbEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.breadCrumbEdit1.Properties.ImageIndex = 1;
-            this.breadCrumbEdit1.Properties.Images = this.sharedImageCollection1;
+            this.breadCrumbEdit1.Properties.Images = this.svgImageCollection1;
             breadCrumbNode1.Caption = "Root";
             breadCrumbNode1.Persistent = true;
             breadCrumbNode1.PopulateOnDemand = true;
@@ -60,12 +59,11 @@
             breadCrumbNode1,
             breadCrumbNode2});
             this.breadCrumbEdit1.Properties.RootImageIndex = 1;
-            this.breadCrumbEdit1.Properties.SelectedNode = breadCrumbNode2;
             this.breadCrumbEdit1.Properties.PathChanged += new DevExpress.XtraEditors.BreadCrumbPathChangedEventHandler(this.breadCrumbEdit1_Properties_PathChanged);
             this.breadCrumbEdit1.Properties.QueryChildNodes += new DevExpress.XtraEditors.BreadCrumbQueryChildNodesEventHandler(this.breadCrumbEdit1_Properties_QueryChildNodes);
             this.breadCrumbEdit1.Properties.ValidatePath += new DevExpress.XtraEditors.BreadCrumbValidatePathEventHandler(this.breadCrumbEdit1_Properties_ValidatePath);
             this.breadCrumbEdit1.Properties.NewNodeAdding += new DevExpress.XtraEditors.BreadCrumbNewNodeAddingEventHandler(this.breadCrumbEdit1_Properties_NewNodeAdding);
-            this.breadCrumbEdit1.Size = new System.Drawing.Size(508, 22);
+            this.breadCrumbEdit1.Size = new System.Drawing.Size(1016, 40);
             this.breadCrumbEdit1.TabIndex = 0;
             this.breadCrumbEdit1.PathChanged += new DevExpress.XtraEditors.BreadCrumbPathChangedEventHandler(this.breadCrumbEdit1_PathChanged);
             // 
@@ -76,44 +74,41 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 46);
+            this.label1.Location = new System.Drawing.Point(18, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.Size = new System.Drawing.Size(139, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Current path:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 46);
+            this.label2.Location = new System.Drawing.Point(176, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(101, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Path Text";
             // 
-            // sharedImageCollection1
+            // svgImageCollection1
             // 
-            // 
-            // 
-            // 
-            this.sharedImageCollection1.ImageSource.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("sharedImageCollection1.ImageSource.ImageStream")));
-            this.sharedImageCollection1.ImageSource.Images.SetKeyName(0, "loadfrom_16x16.png");
-            this.sharedImageCollection1.ImageSource.Images.SetKeyName(1, "open_16x16.png");
-            this.sharedImageCollection1.ParentControl = this;
+            this.svgImageCollection1.Add("imageload", "image://svgimages/dashboards/imageload.svg");
+            this.svgImageCollection1.Add("open", "image://svgimages/actions/open.svg");
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 108);
+            this.ClientSize = new System.Drawing.Size(1064, 208);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.breadCrumbEdit1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.breadCrumbEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sharedImageCollection1.ImageSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sharedImageCollection1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,7 +120,7 @@
         private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private DevExpress.Utils.SharedImageCollection sharedImageCollection1;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
     }
 }
 
